6376bf25084f36c881be3e25a0ea8ac17675a6d7
Stephen Brownsey <55989729+Brownsey@users.noreply.github.com>
2024-04-11 11:22:24 +0100

    Add files via upload

---
+++ Questions.py
@@ -0,0 +1,90 @@
+"""
+Development Practice:
+
+It is important to follow standardised software development practices as much as possible to keep development consistent and reduce chance of code errors.
+During development you should add and commit your code frequently, this will allow you and easy way to revert to a previous version if you break something.
+Once you have finished development of a given issue and you have tested it and feel it is ready to be added to the main branch. 
+You will need to open a pull (merge) request.
+This basically tells the rest of the developers in your team that you are happy with your code and want to migrate it to the main branch.
+This code will be reviewed by at least one other developer. If they are happy with your code, then they can approve this pull request.
+When it is approved, your code will be added to the main branch and your development branch will be deleted (you can keep it in settings if you want but it’s redundant and it’s good practice to delete them once they are merged into master).
+If they are unhappy with your code, your pull request will be rejected with a comment. Once you fix this issue, you can open a new pull request and again your code will be reviewed.
+ 
+
+Coding practices:
+It is important to try and follow standard coding practices when developing on larger projects as this will allow anyone to pick up your code and change it in the future.
+For R: https://style.tidyverse.org/
+For Python: https://docs.python-guide.org/writing/style/
+"""
+
+"""
+Git:
+
+Git uses commands in order control how the code both locally and stored online:
+If you are running the commands then they will need to be run in the terminal section of your IDE unless using a git gui which some IDEs have.
+
+Clone a repository – git clone url - you can get this url fromthe repository
+
+Pull a branch – git pull (can specify a branch but not needed most of the time as it will default to the main branch)
+Create a branch - git checkout -b new_branch_name
+Add your changes – git add . (if you just want to add a specific file, type that instead of .)
+Commit your changes – git commit -m “commit message”
+Push your changes – git push
+
+Extra commands:
+Switch branches - git checkout branch_name  (switches to a different, already created branch)
+If you break stuff to reset to last pushed code – git reset --hard (this will nuke all your changes so if you need to keep any code make sure you copy it beforehand)
+
+There is a lot more functionality that Git can offer, however this is what you will need for 95+% of all your Git interactions and is a good starting point
+"""
+print("Git command questions")
+
+print("Question: What command would you use to clone a repository? \b")
+print("Answer")
+print("")
+
+print("Question: What command would you use to create a new branch called? \b")
+print("Answer")
+print("")
+
+print("Question: What command would you use to add your local changes? \b")
+print("Answer")
+print("")
+
+print("Question: What command would you use to commit these changes with a commit message of changes for demo \b")
+print("Answer")
+print("")
+
+print("Question: What command would you use to push these changes from your local to the remote online \b")
+print("Answer")
+print("")
+
+print("Question: if you messed up, how would you reset your local version to the previous commit? \b")
+print("Answer")
+print("")
+
+print("Question: What command would you type in order to switch from your current branch to one called answers? \b")
+print("Answer")
+print("")
+print("")
+
+print("Extra Questions on Coding practices")
+print("Question: Once you are happy with your code additions, what should you do? \b")
+print("Answer")
+print("")
+
+print("Question: What is the use case of the main (master) branch? \b")
+print("Answer")
+print("")
+
+print("Question: What issue do we avoid by using personal branches for our code? \b")
+print("Answer")
+print("")
+
+print("Question: What is the name for python best coding practices? \b")
+print("Answer")
+print("")
+
+print("Question: What is the name for python best coding practices? \b")
+print("Answer")
+print("")
